e0c7416122c950a4666cae2156f08964ebf20b20
Bernd Kursawe <bernd_kursawe@gmx.de>
2024-07-30 06:04:20 +0200

    new inline class FieldIndex

---
+++ src/main/kotlin/de/praxisit/muli/board/FieldIndex.kt
@@ -0,0 +1,15 @@
+package de.praxisit.muli.board
+
+@JvmInline
+value class FieldIndex(val index: Int) {
+    init {
+        require(index in INDEX_RANGE)
+    }
+
+    companion object {
+        const val SIZE = 24
+        val INDEX_RANGE = 0..<SIZE
+        val INDEXES = INDEX_RANGE.map { it.asFieldIndex }
+        val Int.asFieldIndex get() = FieldIndex(this)
+    }
+}
